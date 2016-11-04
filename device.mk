301103e4f2c2114c2ed56a52c5174d87c659140c
Arne Coucheron <arco68@gmail.com>
2016-11-04 06:21:37 +0100

    yellowstone: Import shim for libwvm.so
    Change-Id: Iee077ad990702e8e34b546df0f00ffdce4fc07c8
    
    yellowstone: libshims: Fix libshim_wvm
    
    _ZN7android16MediaBufferGroupC1Ej() expects an argument. If we don't
    pass one, some undefined value will be used. Pass 0 since that's
    equivalent to calling the old _ZN7android16MediaBufferGroupC1Ev().
    
    Change-Id: If8c917fcf5852666c649c25e9f98965bfc29692f

--- device.mk
+++ device.mk
@@ -166,6 +166,10 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     rm-wrapper
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_wvm
+
 # System Properties
 -include $(LOCAL_PATH)/system_prop.mk
 
