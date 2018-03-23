ba7f72a22e124f57850e205410421517ae3f13d4
Michael Bestas <mkbestas@lineageos.org>
2018-03-23 21:23:39 +0200

    yellowstone: Convert lights HAL into a native binderized HAL
    * Our max brightness is 255.
    * Our backlight path is different on Tegra.
    
    Change-Id: I86a6a2506db2f09aeab484f30a336818895bcdaa

--- device.mk
+++ device.mk
@@ -99,8 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.tegra
+    android.hardware.light@2.0-service.yellowstone
 
 # NFC
 PRODUCT_PACKAGES += \
