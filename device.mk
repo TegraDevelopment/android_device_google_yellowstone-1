0378e39edca307ab33759ad24df1f68c5d97ef66
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-09 11:47:12 +0000

    yellowstone: Add media codecs/profiles
    * Pulled from stock.
    * Opted to include Google media codecs/profiles as well, as this is technically a
      Google-made device.
    
    Change-Id: Ib2bb017a5ba1da9a807178208e54434ee5974596

--- device.mk
+++ device.mk
@@ -47,6 +47,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/camera/nvcamera.conf:system/etc/nvcamera.conf
 
+# Codecs
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codec$
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_c$
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codec$
+    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
