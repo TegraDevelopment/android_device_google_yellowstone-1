a41b2aeb9b3d2d8091d788fd72871dfccbe282ee
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-29 20:24:54 -0600

    yellowstone: Add device/platform specific properties
    * These were pulled from stock (relevant getprop: https://hastebin.com/asogimutop.md),
      and these aren't common props, meaning our device likely relies on them.
    
    * Use system_prop.mk as opposed to system.prop, as that's the accepted way of doing
      things as of AOSP 8.0.
    
    Change-Id: If26526f38174a7680c2f95c6e983fb5575b86728

--- system_prop.mk
+++ system_prop.mk
@@ -14,3 +14,49 @@
 # limitations under the License
 
 ### System Properties for yellowstone
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.override_null_lcd_density=1
+    ro.sf.lcd_density=320
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# GL
+# Use glcomposer as the default compositor
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.compositor=glcomposer
+
+# HWUI Parameters
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.drop_shadow_cache_size=4.0 \
+    ro.hwui.gradient_cache_size=0.8 \
+    ro.hwui.layer_cache_size=32.0 \
+    ro.hwui.path_cache_size=24.0 \
+    ro.hwui.shape_cache_size=3.0 \
+    ro.hwui.text_small_cache_height=512 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_large_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.texture_cache_size=48.0
+
+# NVMM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.nvmmlite=1
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nv.usb.pid.mtp.adb=4D02 \
+    ro.nv.usb.pid.mtp=4D01 \
+    ro.nv.usb.pid.ptp.adb=4D06 \
+    ro.nv.usb.pid.ptp=4D05 \
+    ro.nv.usb.pid.rndis.adb=4D04 \
+    ro.nv.usb.pid.rndis=4D03 \
+    ro.nv.usb.vid=18d1
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
