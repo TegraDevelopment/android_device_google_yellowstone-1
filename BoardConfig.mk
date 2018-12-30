f828fccabeb8f5b511f37f88b82a0efc855491b6
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-29 20:25:49 -0600

    yellowstone: Define Tegra Platform Version
    * LineageOS's NVidia HALs rely on this being set.
    * We have a Tegra K1 (32-bit), not the Tegra K1 (64-bit, also
      known as "denver") so t124 is our Tegra version.
    
    Change-Id: I145397f0795e3170ac623e0a19252e2a7a67ee2a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a15
+TARGET_TEGRA_VERSION := t124
 
 # Board
 TARGET_BOARD_PLATFORM := tegra
