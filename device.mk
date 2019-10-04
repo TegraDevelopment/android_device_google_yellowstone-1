87a0d7b42c6fa4c976f63916080caa786b4e4472
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    yellowstone: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- device.mk
+++ device.mk
@@ -241,7 +241,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwpa_client \
     wificond \
