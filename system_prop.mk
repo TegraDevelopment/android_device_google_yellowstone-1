c3894d654cb17167413bdab82b7fb7a88f0f75e5
Nolen Johnson <johnsonnolen@gmail.com>
2019-11-09 23:02:04 +0000

    yellowstone: Import device specific properties
    * These were pulled from stock (relevant getprop: https://gist.github.com/npjohnson/dee2fe8c3724c86c63e28d76e2ff7245),
      and aren't common properties, meaning our device likely relies on them.
    
    * Notes:
     * We're inheriting 3 GB RAM device dalvik heap and HWUI configurations from
       frameworks/native, but no dalvik heap size avaialable even close to matches
       our specifications, being 7" and 4G RAM, so we may need to use stock's values
       or recalculate them.
     * Advertising GLES Version provides a wider and more accurate compatibillity
       with apps and games in the Play Store.
     * DRM Service is enabled, as our device supports Widevine L3 (software DRM
       decoding).
     * NVMM is "NVidia Multimedia" - a non-standard feature our device supports
       to back NVidia's GStreamer multimedia framework, OMX, and Camera among
       other things. So we set the property stock sets to advertise compatibillity.
     * We set default Wi-Fi and AP interfaces, as all devices need to do this.
     * We set the USB IDs, as stock sets them.
     * Set legacy OMX properties, as Nougat blobs need these.
    
    Signed-off-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: If26526f38174a7680c2f95c6e983fb5575b86728
    
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Vitaliy Tomin <highwaystar.ru@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -13,3 +13,70 @@
 # limitations under the License
 
 ### System Properties for yellowstone
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.config.persist_cal_path=/persist \
+    persist.config.calibration_fac=/sdcard/config/calibration.xml
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.override_null_lcd_density=1 \
+    ro.sf.lcd_density=320
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# GL
+# Use glcomposer as the default compositor
+# Don't preload OpenGL in Zygote, the Tegra drivers do not like it
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.compositor=glcomposer \
+    ro.opengles.version=196608 \
+    ro.zygote.disable_gl_preload=true
+
+# NVMM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.tegra.nvmmlite=1
+
+# OMX
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.less-secure=true \
+    media.stagefright.legacyencoder=true
+
+# PBC
+PRODUCT_PROPERTY_OVERRIDES += \
+    pbc.enabled=0 \
+    pbc.log=0 \
+    pbc.board_power_threshold=20000 \
+    pbc.low_polling_freq_threshold=1000 \
+    pbc.rails=cpu,core,dram,gpu \
+    pbc.cpu.power=/sys/bus/i2c/devices/7-0045/power1_input \
+    pbc.cpu.cap=/dev/cpu_freq_max \
+    pbc.cpu.cap.af=/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies \
+    pbc.core.power=/sys/bus/i2c/devices/7-0043/power1_input \
+    pbc.dram.power=/sys/bus/i2c/devices/7-0049/power1_input \
+    pbc.gpu.power=/sys/bus/i2c/devices/7-004b/power1_input \
+    pbc.gpu.cap=/dev/gpu_freq_max \
+    pbc.gpu.cap.af=/sys/devices/platform/host1x/gk20a.0/devfreq/gk20a.0/available_frequencies
+
+# Senors
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sensorhubd.update=1
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nv.usb.pid.mtp.adb=4D02 \
+    ro.nv.usb.pid.mtp=4D01 \
+    ro.nv.usb.pid.ptp.adb=4D06 \
+    ro.nv.usb.pid.ptp=4D05 \
+    ro.nv.usb.pid.rndis.adb=4D04 \
+    ro.nv.usb.pid.rndis=4D03 \
+    ro.nv.usb.vid=18d1
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    ap.interface=wlan0 \
+    persist.wlan.ti.calibrated=0 \
+    wifi.interface=wlan0
