300cdc3f9d3e7ef16aec14525cf066157ee1efcf
Nolen Johnson <johnsonnolen@gmail.com>
2019-12-16 06:24:06 +0000

    yellowstone: Copy vendor seccomp policy to vendor partion
    Bug: 34723744
    Test: Dragon builds and boots.
    Test: Watch cat videos on youtube no seccomp violations observed.
    Test: For both mediacodec and mediaextractor verify
    "cat proc/<pid>/status | grep Seccomp" == "Seccomp: 2"
    
    Change-Id: I5c1c7fa0facd9afc32ccc2437bb9d2a9641d5d95

--- device.mk
+++ device.mk
@@ -163,6 +163,11 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     rm-wrapper
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Shims
 PRODUCT_PACKAGES += \
     libshim_wvm
