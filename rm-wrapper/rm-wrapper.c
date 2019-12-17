3ae7296c4ef46b34aac18372d0b5183e266cf98c
Aaron Kling <webgeek1234@gmail.com>
2019-12-17 22:59:33 +0000

    yellowstone: Update rm-wrapper for 8.0
    Change-Id: Ib0bfdeb64451b2687bcff18b364cffe793d02111

--- rm-wrapper/rm-wrapper.c
+++ rm-wrapper/rm-wrapper.c
@@ -49,6 +49,38 @@ int __android_log_print(int prio, const char *tag, const char *fmt, ...) {
 
 #endif
 
+int str_between(const char* str, const char* prefix, const char* suffix, char* output, size_t out_size) {
+    size_t str_len = strlen(str);
+    size_t prefix_len = strlen(prefix);
+    size_t suffix_len = strlen(suffix);
+
+    if (strncmp(str, prefix, prefix_len) == 0 &&
+        strncmp(str + str_len - suffix_len, suffix, suffix_len) == 0) {
+        size_t out_len = min(out_size - 1, str_len - prefix_len - suffix_len);
+        if (output != NULL) {
+            strncpy(output, str + prefix_len, out_len);
+            output[out_len] = 0;
+        }
+        return out_len;
+    } else return -1;
+}
+
+unsigned char* POWER_MAGIC = "";
+
+FILE* fopen(const char* path, const char* mode) {
+    static FILE* (*_fopen)(const char*, const char*) = NULL;
+    if (!_fopen)
+        _fopen = dlsym(RTLD_NEXT, "fopen");
+
+    if (str_between(path, "/sys/class/power_supply/", "/online", NULL, 0) >= 0) {
+        FILE* ret = malloc(sizeof(FILE));
+        ret->_p = POWER_MAGIC;
+        return ret;
+    }
+
+    return _fopen(path, mode);
+}
+
 int (*ts_main_init)();
 int (*ts_main_resume)();
 int (*ts_main_calc)();
@@ -59,7 +91,7 @@ int main() {
         printf("Failed to load library: %s\n", dlerror());
         return 1;
     }
- 
+
     ts_main_init = dlsym(dl, "ts_main_init");
     if (!ts_main_init) {
         printf("Failed to find ts_main_init: %s\n", dlerror());
